3758365652c52692de4ff980e33f8b451dd27eba
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-02 01:48:05 -0500

    Add query What animals belong to Melody Pond?

--- queries.sql
+++ queries.sql
@@ -122,3 +122,11 @@ SELECT species, AVG(escape_attempts)
 FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
+
+/* What animals belong to Melody Pond?*/
+
+SELECT a.name as animal, o.full_name as owner
+FROM animals a
+JOIN owners o
+ON a.owner_id = o.id
+WHERE o.full_name = 'Melody Pond';
