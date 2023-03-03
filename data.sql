2e2cf0cfd2596656f04581e2b041e6a9a0b9bed1
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-03-03 05:39:14 -0500

    Insert data for vets

--- data.sql
+++ data.sql
@@ -73,3 +73,14 @@ SET species_id =
     END;
 
 select * from animals;  
+
+/* Vet clinic database: add "join table" for visits */
+
+/* Insert the following data for vets */
+
+INSERT INTO vets (name, age, date_of_graduation) 
+VALUES 
+    ('William Tatcher', 45, '2000-04-23'),
+    ('Maisy Smith', 26, '2019-01-17'),
+    ('Stephanie Mendez', 64, '1981-05-04'),
+    ('Jack Harkness', 38, '2008-06-08');
